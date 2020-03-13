e77d02a980d2ca6265e2709d16207bd7b1de1621
Marko Zivko <marko.zivko@bellabeat.com>
2020-03-13 08:59:13 +0100

    New question type added

---
+++ UpitnikDemo/UpitnikDemo/BirthDateViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BirthDateViewController.swift
+//  UpitnikDemo
+//
+//  Created by Marko Zivko on 12/03/2020.
+//  Copyright © 2020 Marko Zivko. All rights reserved.
+//
+
+import UIKit
+
+class BirthDateViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
